b16e46b386e33b8c44e02ff9179a4c5a9280ce60
Omichpolyebok <184048278+Omichpolyebok@users.noreply.github.com>
2025-12-08 15:48:34 +0500

    Update SMTP configuration with new credentials

--- src/config.php
+++ src/config.php
@@ -1,9 +1,9 @@
 <?php
 return [
   'smtp_host'   => 'smtp.yandex.ru',
-  'smtp_user'   => 'Omichp@yandex.com',
-  'smtp_pass'   => 'hfaygubnbmeuyloi',
-  'smtp_port'   => 465,               // или 587, см. ниже
-  'from_email'  => 'Omichp@yandex.com',
+  'smtp_user'   => 'youremail',
+  'smtp_pass'   => 'yourpass',
+  'smtp_port'   => 465,             
+  'from_email'  => 'youremail',
   'from_name'   => 'MySite',
 ];
